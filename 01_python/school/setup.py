943ba22a62e011c9d1540ddcf60a69fe5a7c11c8
hunsoo0823 <hunsoo0823@naver.com>
2021-02-28 23:41:45 +0900

    2week 4day,5day

---
+++ 01_python/school/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "dss",
+    packages = find_packages(),
+    include_pakage_data = True,
+    version = "0.0.1",
+    author = "HyeonsooSong",
+    author_email="hunsoo0823@naver.com",
+    zip_safe = False,
+)
